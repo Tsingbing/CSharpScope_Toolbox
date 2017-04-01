b8cef4b0bdf4becf3a9f3c50062455caa5758af4
dfoldesi <dalma@localprojects.net>
2017-03-31 23:44:47 -0400

    Real-time color separation

--- GridDecoder/Assets/Scanners.cs
+++ GridDecoder/Assets/Scanners.cs
@@ -28,6 +28,9 @@ public class Scanners : MonoBehaviour
 	public float xOffset = 0;
 	public float zOffset = 0;
 
+	// red, black, white, gray
+	private Vector3[] colors = new Vector3[] { new Vector3(1f, 0f, 0f), new Vector3(1f, 1f, 1f), new Vector3(0f, 0f, 0f), new Vector3(0.5f, 0.5f, 0.5f)};
+
 
 	IEnumerator Start ()
 	{
@@ -46,34 +49,23 @@ public class Scanners : MonoBehaviour
 			Debug.Log ("Z offset is " + zOffset);
 		}
 
+		_texture = new Texture2D (GetComponent<Renderer> ().material.mainTexture.width, 
+			GetComponent<Renderer> ().material.mainTexture.height);
 	
 		while (true) {
 
 			yield return new WaitForEndOfFrame ();
 
-			_texture = new Texture2D (GetComponent<Renderer> ().material.mainTexture.width, 
-				                     GetComponent<Renderer> ().material.mainTexture.height);
-
-			if (_useWebcam) {
-				_texture.SetPixels ((GetComponent<Renderer> ().material.mainTexture as WebCamTexture).GetPixels ()); //for webcam 
-			}
-			else {
-				_texture.SetPixels ((GetComponent<Renderer> ().material.mainTexture as Texture2D).GetPixels ()); // for texture map 
-			};
-				
-			_texture.Apply (); //need fixing!!!
+			setTexture ();
 
 			yield return new WaitForSeconds (_refreshRate);
 
+			// Assign render texture from keystoned quad texture copy & copy it to a Texture2D
+			Texture2D hitTex = assignRenderTexture();
+
 			for (int i = 0; i < scannersList.Count; i++) {
 				if (Physics.Raycast (scannersList [i].transform.position, Vector3.down, out hit, 6)) {
-						// Get local tex coords w.r.t. triangle
-
-					RenderTexture rt = hit.transform.GetComponent<Renderer> ().material.mainTexture as RenderTexture;
-					RenderTexture.active = rt;
-					Texture2D hitTex = new Texture2D (rt.width, rt.height, TextureFormat.RGB24, false);
-
-					hitTex.ReadPixels( new Rect(0, 0, rt.width, rt.height), 0, 0);
+					// Get local tex coords w.r.t. triangle
 
 					if (!hitTex) {
 						Debug.Log ("No hit texture");
@@ -82,16 +74,17 @@ public class Scanners : MonoBehaviour
 					else {
 						int _locX = Mathf.RoundToInt (hit.textureCoord.x * hitTex.width - xOffset);
 						int _locY = Mathf.RoundToInt (hit.textureCoord.y * hitTex.height - zOffset); 
-						Color pixel = hitTex.GetPixel (_locX, _locY); 
-						scannersList [i].GetComponent<Renderer> ().material.color = pixel; //paint scanner with the found color 
+						Color pixel = hitTex.GetPixel (_locX, _locY);
+						pixel = closestColor (pixel);
+
+						//paint scanner with the found color 
+						scannersList [i].GetComponent<Renderer> ().material.color = pixel;
+
 						if (_showRays) {
 							Debug.DrawLine (scannersList [i].transform.position, hit.point, pixel, 200, false);
 							Debug.Log (hit.point);
 						}
-
-						RenderTexture.active = null;
 					}
-
 				} else { 
 					scannersList [i].GetComponent<Renderer> ().material.color = Color.magenta; //paint scanner with Out of bounds  color 
 				}
@@ -99,6 +92,56 @@ public class Scanners : MonoBehaviour
 		}
 	}
 
+	/// <summary>
+	/// Assigns the render texture to a Texture2D.
+	/// </summary>
+	/// <returns>The render texture as Texture2D.</returns>
+	private Texture2D assignRenderTexture() {
+		RenderTexture rt = GameObject.Find ("KeystonedTextureQuad").transform.GetComponent<Renderer> ().material.mainTexture as RenderTexture;
+		RenderTexture.active = rt;
+		Texture2D hitTex = new Texture2D (rt.width, rt.height, TextureFormat.RGB24, false);
+		hitTex.ReadPixels( new Rect(0, 0, rt.width, rt.height), 0, 0);
+		return hitTex;
+	}
+
+	/// <summary>
+	/// Sets the texture.
+	/// </summary>
+	private void setTexture() {
+		if (_useWebcam) {
+			_texture.SetPixels ((GetComponent<Renderer> ().material.mainTexture as WebCamTexture).GetPixels ()); //for webcam 
+		}
+		else {
+			_texture.SetPixels ((GetComponent<Renderer> ().material.mainTexture as Texture2D).GetPixels ()); // for texture map 
+		};
+		_texture.Apply ();
+	}
+
+	/// <summary>
+	/// Finds the closest color to the given scan colors.
+	/// </summary>
+	/// <returns>The closest color found.</returns>
+	/// <param name="pixel">Pixel.</param>
+	private Color closestColor(Color pixel) {
+		Vector3 currPixel = new Vector3 (pixel.r, pixel.g, pixel.b);
+		float minDistance = 1000;
+		Vector3 minColor = new Vector3 (0f, 0f, 0f);
+
+		for (int i = 0; i < colors.Count (); i++) {
+			float currDistance = Vector3.Distance (colors [i], currPixel);
+			if (currDistance < minDistance) {
+				minDistance = currDistance;
+				minColor = colors [i];
+			}
+		}
+
+		Color closestColor = new Color (minColor.x, minColor.y, minColor.z);
+		return closestColor;
+	}
+
+	/// <summary>
+	/// Initialize scanners.
+	/// </summary>
 	private void scannersMaker ()
 	{
 		for (int x = 0; x < _numOfScannersX; x++) {
